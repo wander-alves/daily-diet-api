7406277aa00318dde7a3d456de9618252de5c5d9
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-06-15 00:15:22 -0300

    feat: creating knex setup

---
+++ src/database/index.ts
@@ -0,0 +1,17 @@
+import { Knex, knex as knexClient } from "knex";
+import { env } from '../infra/env';
+
+const { DB_CLIENT, DB_URL } = env;
+
+const DB_CONNECTION = DB_CLIENT === 'pg' ? DB_URL : {
+  filename: DB_URL
+}
+
+const config: Knex.Config = {
+  client: env.DB_CLIENT,
+  connection: DB_CONNECTION,
+};
+
+const knex = knexClient(config);
+
+export { knex, config }
